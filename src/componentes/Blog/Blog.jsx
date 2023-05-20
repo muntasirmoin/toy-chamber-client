d0f992cf7c7ceac85c79e4730cb5bbf0191f730e
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2023-05-21 00:55:43 +0600

    q1 added

--- src/componentes/Blog/Blog.jsx
+++ src/componentes/Blog/Blog.jsx
@@ -12,7 +12,7 @@ const Blog = () => {
 
             <div class="bg-gray-900 p-4">
                 <div class="text-white">
-                    <h1 class="text-2xl font-bold">Title</h1>
+                    <h1 class="text-2xl font-bold">1. What is an access token and refresh token? How do they work and where should we store them on the client-side?</h1>
                     <p class="text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend quam sit amet semper faucibus. Morbi pellentesque mattis sem sed cursus. Integer bibendum tellus eu libero mollis, id vestibulum ex faucibus. Fusce ullamcorper neque et augue aliquam, et fringilla urna volutpat. Vestibulum pharetra justo nisl, vitae finibus ante blandit a. Nunc a efficitur leo, ut placerat elit. Aliquam tincidunt lectus vel lorem pharetra vulputate. Aliquam et ultricies nunc, eu ultrices lectus. Ut in nulla sem. Cras non arcu ac sem vestibulum dignissim. Fusce elementum congue diam, in lobortis erat fringilla in. Sed lobortis nisl et magna dignissim, eu viverra tortor pellentesque.</p>
                 </div>
             </div>
