778612b8bab06b14853f0b70123e59a7f871423d
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2023-05-21 01:35:59 +0600

    Footer done

--- src/componentes/shared/Footer/Footer.jsx
+++ src/componentes/shared/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  // footer complete
     return (
         <div>
             <footer className="bg-gray-900 text-gray-300 py-8">
